2a8a5a9f4c681e54bc0489bced9528f5906d889f
SarahVargas79 <rafaelav4521@gmail.com>
2025-04-15 11:03:21 -0300

    atualização na Aula04, incluído servidor

--- Aula04-JSON/script.js
+++ Aula04-JSON/script.js
@@ -21,7 +21,7 @@ function lerJSON() {
 
             txt += "<br>Opcionais: ";
             objJSON.opcionais.forEach(op => {
-                txt += "<br>" + op.nome + "Marca" + op.marca;
+                txt += "<br> •" + op.nome + "<br>Marca: " + op.marca;
             });
 
             document.getElementById("divJSON").innerHTML = txt;
@@ -32,4 +32,93 @@ function lerJSON() {
     };
     req.open("GET", "dados.json", true);
     req.send()
+}
+
+function consultar() {
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var objJSON = JSON.parse(this.responseText);
+            if (objJSON.resposta) {
+                alert(objJSON.resposta);
+            } else {
+                txt = '<table border="1">';
+                txt += '<tr>';
+                txt += '    <th>Id</th>';
+                txt += '    <th>Nome</th>';
+                txt += '    <th>Preço</th>';
+                txt += '    <th>Ação</th>';
+                txt += '</tr>';
+                objJSON.produtos.forEach(prod => {
+                    txt += '<tr>';
+                    txt += '    <td>' + prod.id + '</td>';
+                    txt += '    <td>' + prod.nome + '</td>';
+                    txt += '    <td>' + prod.preco + '</td>';
+                    txt += '    <td><button onclick="excluir(' + prod.id + ')"><i class="fas fa-trash"></i></button></td>';
+
+                    txt += '</tr>';
+                });
+                txt += '</table>';
+                document.getElementById("divProdutos").innerHTML = txt;
+            }
+
+        } else if (this.readyState == 4) {
+            alert(this.status + " - " + this.statusText);
+        }
+
+    }
+    req.open("GET", "servidor.php?consultar", true);
+    req.send()
+}
+
+function salvar() {
+    var nome = document.getElementById("txtNome").value
+    if (nome == "") alert("O campo do nome é OBRIGATÓRIO");
+    else {
+        var preco = document.getElementById("txtPreco").value;
+        vPreco = 0.0;
+        if (preco != "") {
+            preco = preco.replace(",", ".");
+            vPreco = parseFloat(preco);
+        }
+        var req = new XMLHttpRequest();
+
+        req.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var objJSON = JSON.parse(this.responseText);
+                if (objJSON.resposta) {
+                    alert(objJSON.resposta);
+                } else {
+                    alert("Id gerado: " + objJSON.id);
+                    consultar();
+                }
+
+            } else if (this.readyState == 4) {
+                alert(this.status + " - " + this.statusText);
+            }
+        };
+        req.open("POST", "servidor.php?inserir", true);
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.send("name=" + nome + "&price=" + vPreco);
+    }
+}
+
+function excluir(id) {
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var objJSON = JSON.parse(this.responseText);
+            if (objJSON.resposta) {
+                alert(objJSON.resposta);
+                consultar();
+            }
+        } else if (this.readyState == 4) {
+            alert(this.status + " - " + this.statusText);
+        }
+    };
+
+    req.open("GET", "servidor.php?deletar&produto=" + id, true);
+    req.send();
 }
---
+++ Aula04-JSON/servidor.php
@@ -0,0 +1,64 @@
+<?php
+
+header("Content-type: application/json");
+
+$local = "localhost";
+$user = "root";
+$senha = "";
+$banco = "market";
+
+if (isset($_REQUEST["consultar"])) {
+    try {
+        $conn = mysqli_connect("$local", "$user", "$senha", "$banco");
+        if ($conn) {
+            $sql = "SELECT * FROM produto ORDER BY id ASC";  // ASC ordena em ordem crescente;
+            $result = mysqli_query($conn, $sql);
+            $linhas = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                $linhas[] = $row;
+            }
+            mysqli_close($conn);
+            echo '{"produtos": ' . json_encode($linhas) . '}';
+        } else {
+            echo ' {"resposta": "Erro ao tentar conectar"} ';
+        }
+    } catch (\Throwable $th) {
+        echo ' {"resposta": "Erro no servidor"} ';
+    }
+}
+
+if (isset($_REQUEST["inserir"])) {
+    $nome = $_POST["name"];
+    $preco = $_POST["price"];
+    try {
+        $conn = mysqli_connect("$local", "$user", "$senha", "$banco");
+        if ($conn) {
+            $sql = "INSERT INTO produto (nome, preco) VALUES ('$nome', $preco)";
+            mysqli_query($conn, $sql);
+            $id = mysqli_insert_id($conn);
+            mysqli_close($conn);
+            echo '{"id": ' . $id . '}';
+        } else {
+            echo ' {"resposta": "Erro ao tentar conectar"} ';
+        }
+    } catch (\Throwable $th) {
+        echo ' {"resposta": "Erro no servidor"} ';
+    }
+}
+
+if (isset($_REQUEST["deletar"])) {
+    $id = $_GET["produto"];
+    try {
+        $conn = mysqli_connect("$local", "$user", "$senha", "$banco");
+        if ($conn) {
+            $sql = "DELETE FROM produto WHERE id = $id";
+            mysqli_query($conn, $sql);
+            mysqli_close($conn);
+            echo '{"resposta": "Produto excluído com sucesso!"}';
+        } else {
+            echo '{"resposta": "Erro ao tentar conectar"}';
+        }
+    } catch (\Throwable $th) {
+        echo '{"resposta": "Erro no servidor"}';
+    }
+}
